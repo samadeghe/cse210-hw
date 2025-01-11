1228cce4019fdb8c80cb825cf0f49f8b7e05be0c
samadeghe <sadeghe@byupathway.edu>
2025-01-11 16:28:56 -0500

    week one done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            int magicNumber = randomGenerator.Next(1, 100);
+            int guess = -1;
+
+            Console.WriteLine("Welcome to the Guessing Game!");
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            string response = Console.ReadLine().ToLower();
+
+            if (response != "yes")
+            {
+                playAgain = false;
+                Console.WriteLine("Thanks for playing! Goodbye!");
+            }
+        }
     }
 }
